c01d06e1895f3f9a6be1f06739d727e1169f279b
Bryan Tacuri <bryantacuriar@gmail.com>
2022-06-18 10:35:35 -0500

    Refactoring
    Refactoring y control de errores

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -14,66 +14,92 @@ use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 
-
-
 class CategoryService
 {
     public function getAll()
     {
-        if ((auth()->user())) {
-            return Category::paginate();
-        } else {
-            return Category::where('status', 'A')->paginate(10);
+        try {
+            if ((auth()->user())) {
+                return Category::paginate();
+            } else {
+                return Category::where('status', 'A')->paginate(10);
+            }
+        } catch (\Exception $e) {
+            return $e->getMessage();
         }
     }
 
     public function create($data)
     {
-        $validator = Validator::make($data->all(), [
-            'name' => 'required|string|max:255',
-            'user_create' => 'required',
-        ]);
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        } else {
-            $category = Category::create($data->all());
-            return $category;
+        try {
+            $validator = Validator::make($data->all(), [
+                'name' => 'required|string|max:255',
+                'user_create' => 'required',
+            ]);
+            if ($validator->fails()) {
+                $jsonErrors =  $validator->errors();
+                $error =  json_decode($jsonErrors, TRUE);
+                return $error;
+            } else {
+                $category = Category::create($data->all());
+                return $category;
+            }
+        } catch (\Exception $e) {
+            return $e->getMessage();
         }
     }
 
     public function getId($id)
     {
-        $category = Category::find($id);
-        return $category;
+
+        try {
+            if ((auth()->user())) {
+                return Category::find($id);
+            } else {
+                return Category::where('id', $id)->where('status', 'A')->get();
+            }
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
     }
-    public function update($request, $id)
+    public function update($data, $id)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'user_modifies' => 'required',
-        ]);
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        } else {
-            $category = Category::findOrFail($id);
-            $category->update($request->all());
-            return $category;
+        try {
+            $validator = Validator::make($data->all(), [
+                'name' => 'required|string|max:255',
+                'user_modifies' => 'required',
+            ]);
+            if ($validator->fails()) {
+                $jsonErrors =  $validator->errors();
+                return json_decode($jsonErrors, TRUE);
+            } else {
+                $category = Category::findOrFail($id);
+                $category->update($data->all());
+                return $category;
+            }
+        } catch (\Exception $e) {
+            return $e->getMessage();
         }
     }
 
-    public function delete($request, $id)
+    public function delete($data, $id)
     {
-        $validator = Validator::make($request->all(), [
-            'status' => 'required|string|max:1',
-            'user_delete' => 'required',
-            'date_delete' => 'required',
-        ]);
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        } else {
-            $category = Category::findOrFail($id);
-            $category->update($request->only('status', 'user_delete', 'date_delete'));
-            return $category;
+        try {
+            $validator = Validator::make($data->all(), [
+                'status' => 'required|string|max:1',
+                'user_delete' => 'required',
+                'date_delete' => 'required',
+            ]);
+            if ($validator->fails()) {
+                $jsonErrors =  $validator->errors();
+                return json_decode($jsonErrors, TRUE);
+            } else {
+                $category = Category::findOrFail($id);
+                $category->update($data->only('status', 'user_delete', 'date_delete'));
+                return $category;
+            }
+        } catch (\Exception $e) {
+            return $e->getMessage();
         }
     }
 }
